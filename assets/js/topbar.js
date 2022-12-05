39cee8998b310c9306f2bb8f17e75ee2f26817fe
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-12-06 00:24:42 +0100

    topbar: Rework
    Change-Id: I5ee40a9507952374211076a1941b0da58f302c4f

--- assets/js/topbar.js
+++ assets/js/topbar.js
@@ -1,12 +1,13 @@
-var headElement = document.getElementById("topbar");
+var headElement = document.getElementById("topbar-container");
 var topbarElement = document.createElement("div");
 topbarElement.innerHTML = '' +
-		'<ul class="topbar">' +
-			'<div style="float:left;">' +
+		'<link rel="stylesheet" href="assets/css/topbar.css"/>' +
+		'<ul id="topbar">' +
+			'<div id="topbar-left">' +
 				'<li><img src="https://avatars3.githubusercontent.com/u/38215111?s=42&v=4"></li>' +
 				'<li><a>SebaUbuntu</a></li>' +
 			'</div>' +
-			'<div style="float:right;">' +
+			'<div id="topbar-right">' +
 				'<li><a id="topbar-home" href="index.html">Home</a></li>' +
 				'<li><a id="topbar-blog" href="blog.html">Blog</a></li>' +
 				'<li><a id="topbar-downloads" href="downloads.html">Downloads</a></li>' +
