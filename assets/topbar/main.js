3b874cdfcfc46e611f570c50ff5572dbfaf2f93d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-02-23 00:02:18 +0100

    topbar: Handle root path
    Change-Id: Ic7b4ef115d9699fc516e928bff167243fab3a185

--- assets/topbar/main.js
+++ assets/topbar/main.js
@@ -6,13 +6,13 @@
 let topbarContainerElement = document.getElementById("topbar-container");
 
 const PAGES = {
-	"Home": "/index.html",
-	"Blog": "/blog.html",
-	"Downloads": "/downloads.html",
-	"LineageOS apps": "/lineageapps.html",
-	"Status": "/status.html",
-	"Files": "https://files.sebaubuntu.dev",
-	"Contact": "https://t.me/SebaUbuntu",
+	"Home": ["/", "/index.html"],
+	"Blog": ["/blog.html"],
+	"Downloads": ["/downloads.html"],
+	"LineageOS apps": ["/lineageapps.html"],
+	"Status": ["/status.html"],
+	"Files": ["https://files.sebaubuntu.dev"],
+	"Contact": ["https://t.me/SebaUbuntu"],
 }
 
 function main() {
@@ -69,8 +69,8 @@ function main() {
 			<div class="pages">
 				${Object.keys(PAGES).map(page => `
 					<a
-						class="button ${window.location.pathname === PAGES[page] ? "active" : ""}"
-						href="${PAGES[page]}"
+						class="button ${PAGES[page].includes(window.location.pathname) ? "active" : ""}"
+						href="${PAGES[page][0]}"
 					>
 						${page}
 					</a>
