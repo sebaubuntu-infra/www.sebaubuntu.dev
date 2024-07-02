cb7d5ed2c515f6e55e72d6d1d1eb79c3e3856519
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-02 02:48:11 +0200

    blog: Cleanup
    Change-Id: I94ef88c12a5ab0b84327faaa8238bd401a7b3ae2

--- assets/blog/main.js
+++ assets/blog/main.js
@@ -5,17 +5,7 @@
 
 const PAGES_JSON_PATH = "assets/blog/pages.json";
 
-const dateSvg = `
-	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z"></path>
-	</svg>
-`;
-
-const userSvg = `
-	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM7.07 18.28c.43-.9 3.05-1.78 4.93-1.78s4.51.88 4.93 1.78C15.57 19.36 13.86 20 12 20s-3.57-.64-4.93-1.72zm11.29-1.45c-1.43-1.74-4.9-2.33-6.36-2.33s-4.93.59-6.36 2.33C4.62 15.49 4 13.82 4 12c0-4.41 3.59-8 8-8s8 3.59 8 8c0 1.82-.62 3.49-1.64 4.83zM12 6c-1.94 0-3.5 1.56-3.5 3.5S10.06 13 12 13s3.5-1.56 3.5-3.5S13.94 6 12 6zm0 5c-.83 0-1.5-.67-1.5-1.5S11.17 8 12 8s1.5.67 1.5 1.5S12.83 11 12 11z"></path>
-	</svg>
-`;
+const mainPageElement = document.getElementById("main-page");
 
 class Page {
 	/**
@@ -62,7 +52,12 @@ class Page {
 		div.innerHTML = `
 			<h5 class="post-card-description">${this.description}</h5>
 			<h2 class="post-card-title"><a href="blog.html?page=${this.id}">${this.title}</a></h2>
-			<p class="post-card-info">${dateSvg} ${this.getFormattedDate()} ${userSvg} ${this.author}</p>
+			<p class="post-card-info">
+				<img src="assets/blog/icons/calendar_month.svg" alt="Date">
+				<a>${this.getFormattedDate()}</a>
+				<img src="assets/blog/icons/account_circle.svg" alt="Author">
+				<a>${this.author}</a>
+			</p>
 		`;
 		return div;
 	}
@@ -94,8 +89,7 @@ async function getPages() {
 }
 
 async function main() {
-	const blogMain = document.getElementById("blog-main");
-	blogMain.innerHTML = "";
+	mainPageElement.innerHTML = "";
 
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
@@ -103,7 +97,7 @@ async function main() {
 	let pages = await getPages();
 	if (!pages) {
 		// Add a warning to the user
-		blogMain.innerHTML += `
+		mainPageElement.innerHTML += `
 			<h1 class="warning">Failed to load the blog posts.</h1>
 		`;
 		return;
@@ -115,7 +109,7 @@ async function main() {
 		let page = pages.find(page => page.id == requestedPage);
 		if (!page) {
 			// Add a warning to the user
-			blogMain.innerHTML += `
+			mainPageElement.innerHTML += `
 				<h1 class="warning">The requested post does not exist.</h1>
 				<a href="blog.html" class="back-button">Back</a>
 			`;
@@ -126,7 +120,7 @@ async function main() {
 		let contentHtml = await page.getContentHtml();
 
 		// Add title, description and date
-		blogMain.innerHTML += `
+		mainPageElement.innerHTML += `
 			<div id="post-header">
 				<h1 id="post-header-title">${page.title}</h1>
 				<h4 id="post-header-info">Written on ${page.getFormattedDate()} by ${page.author}</h4>
@@ -138,7 +132,7 @@ async function main() {
 	} else {
 		// The user didn't request a specific page, show the list of pages
 		pages.forEach(page => {
-			blogMain.appendChild(page.getButtonHtml());
+			mainPageElement.appendChild(page.getButtonHtml());
 		});
 	}
 }
